427b479bf9a1627d9da3c8fb80ceab87914bb2c6
Bastian <bastianibarra05@inacapmail.cl>
2023-09-22 12:07:01 -0300

    app2

--- PJOP_proyecto/PJOP_app1/views.py
+++ PJOP_proyecto/PJOP_app1/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def vista1(request):
     html="""
-    <div style="background-color: green"> ESTA ES APP1 VISTA 1</div>
+    <h3 style="background-color: yellow"> ESTA ES APP1 VISTA 1</h3>
     """
     return HttpResponse(html)
 
 def vista2(request):
     html="""
-    <h1 style="background-color: green"> ESTA ES APP1 VISTA 1</h1>
+    <h4 style="background-color: orange"> ESTA ES APP1 VISTA 2</h4>
     """
     return HttpResponse(html)
